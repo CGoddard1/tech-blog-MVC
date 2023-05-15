a813ccb75a0e7adbeeba0e1f676a50f6657f4ae8
CGoddard1 <goddard.cole@gmail.com>
2023-05-15 16:25:42 -0400

    server.js set up and .env

---
+++ server.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Adding in this so that we are able to access session variables in any of our handlebars templates
+// https://stackoverflow.com/questions/44883228/how-to-get-the-express-session-variable-in-all-the-handlebars-pages-right-now-i
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
+
+
+//set up handlebars as the "view engine"
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
